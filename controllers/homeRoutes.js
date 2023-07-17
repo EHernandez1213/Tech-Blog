bc559f7666cb12bfef0d88b323019857084218a2
Whynotwynaut <125845043+EHernandez1213@users.noreply.github.com>
2023-07-16 21:13:08 -0700

    fixed stuff

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,6 +20,7 @@ router.get('/', async (req, res) => {
     // Pass serialized data and session flag into template
     res.render('homepage', {
       posts,
+      
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -85,7 +86,7 @@ router.get('/signUp', (req, res) => {
   res.render('signUp');
 });
 
-route.get('/edit/:id', withAuth, async (req, res) => {
+router.get('/edit/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id)
     if (postData) {
